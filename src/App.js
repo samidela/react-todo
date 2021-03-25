3f8e3beb6a3d8a66348912cef31848516a9cf93b
Samuel Eyob <saeyob78@gmail.com>
2021-03-25 15:07:30 -0500

    now able to add and delete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // Importing Components 
 import Form from "./Components/Form"
 import TodoList from './Components/ToDoList';
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
-      <h1>Sami's ToDo List</h1>
+        <h1>Sami's ToDo List </h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos} />
     </div>
   );
 }
