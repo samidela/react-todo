95bbb0e198401bb338840f79a55ec29bfbc3c0d5
Samuel Eyob <saeyob78@gmail.com>
2021-03-25 15:11:12 -0500

    add todo as a prop

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos }) => {
+    console.log(todos)
     return (
-        <div className="todo-contatiner">
-            <ul className="todo-list"></ul>
+        <div className="todo-container">
+            <ul className="todo-list">.
+                {todos.map((todo) => (
+                    <Todo key={todo.id} text={todo.text} setTodos={setTodos} todos={todos} todo={todo} />
+                ))}
+            </ul>
         </div>
     );
 };
